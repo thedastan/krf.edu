0df23d739b7bedc74fe6c7d243c041df7c7e5cb0
AsimMahmudov <AsimMahmudov>
2024-12-18 12:23:39 +0600

    change

--- src/constants/seo/seo.constants.ts
+++ src/constants/seo/seo.constants.ts
@@ -1,10 +1,10 @@
 export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
-export const SITE_NAME = 'Metallomir.kg'
+export const SITE_NAME = 'Кыргызско-Российский факультет'
 
-export const SEO_KEY_WORDS = 'Metallomir'
-
-
-export const SEO_IMAGE = 'https://kzmc.kg/media/uploads/images/listovoi_prokat1.jpg'
+export const SEO_KEY_WORDS = 'Кыргызско-Российский факультет'
 
 
-export const SEO_DESCRIPTION = '"Metallomir" — надежный поставщик металлопроката и металлопродукции в Кыргызстане.'
+export const SEO_IMAGE = 'http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.aa3b36ce.jpeg&w=1200&q=75'
+
+
+export const SEO_DESCRIPTION = 'Сохранение и развитие культурного наследия нации через подготовку современных квалифицированных специалистов в области филологического образования.'
