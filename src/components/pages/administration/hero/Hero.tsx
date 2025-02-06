09b9ae0fe0b1cba04c6178bc06ab5a1cc75bfb58
AsimMahmudov <AsimMahmudov>
2025-02-06 12:01:18 +0600

    finish pro

--- src/components/pages/administration/hero/Hero.tsx
+++ src/components/pages/administration/hero/Hero.tsx
@@ -2,18 +2,18 @@
 import { useGetAdminQuery } from "@/redux/api/catalog";
 import scss from "./Hero.module.scss";
 import { useRouter } from "next/navigation";
-import { useLanguageStore } from "@/stores/useLanguageStore";
+// import { useLanguageStore } from "@/stores/useLanguageStore";
 
 const Hero = () => {
 	const { data } = useGetAdminQuery();
 
-	const { t } = useLanguageStore();
+	// const { t } = useLanguageStore();
 
 	const router = useRouter();
 	return (
 		<div className={scss.Hero}>
 			<div className="container">
-				<h1 style={{ textAlign: "center" }}>{t("Works.title")}</h1>
+				{/* <h1 style={{ textAlign: "center" }}>{t("Works.title")}</h1> */}
 				<div className={scss.content}>
 					{data?.map((el, index) => (
 						<h1 onClick={() => router.push(`administration/${el.id}`)} key={index}>
